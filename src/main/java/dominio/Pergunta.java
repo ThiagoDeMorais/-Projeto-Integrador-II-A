19c33cced8b0de2aa715133ff056c018d0d7bcea
Thigo De Morais <thiagodevelopermorais@gmail.com>
2023-05-20 10:37:41 -0300

    alteracao

--- src/main/java/dominio/Pergunta.java
+++ src/main/java/dominio/Pergunta.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pergunta {
-	private int id;
+	private int iddddd;
 	private String enunciado;
 	private  List<String> alternativas = new ArrayList<>();
 	
@@ -17,11 +17,11 @@ public class Pergunta {
 	}
 
 	public int getId() {
-		return id;
+		return iddddd;
 	}
 
 	public void setId(int id) {
-		this.id = id;
+		this.iddddd = id;
 	}
 
 	public String getEnunciado() {
